4c0bd4af9a89d25e1a4834c9d6abe4f9dd21db33
Anildo Caldas <anildk5@hotmail.com>
2020-05-27 16:05:14 -0300

    finalized roll dices app

--- dice_app_flutter/lib/main.dart
+++ dice_app_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(MyApp());
 
@@ -22,18 +23,45 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void rollTheDices() {
+    setState(() {
+      this.leftDiceNumber = Random().nextInt(6) + 1;
+      this.rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset("images/dice1.png"),
-        ),
-        Expanded(
-          child: Image.asset("images/dice2.png"),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset("images/dice$leftDiceNumber.png"),
+              onPressed: () {
+                this.rollTheDices();
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset("images/dice$rightDiceNumber.png"),
+              onPressed: () {
+                this.rollTheDices();
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
